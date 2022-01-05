25bb6ced71e82d030cad6c1326a51a75e213cb02
sm6srw <sm6srw@arrl.net>
2022-01-05 11:51:49 -0500

    Add band labels node

---
+++ antenna-genius-band-labels.js
@@ -0,0 +1,53 @@
+module.exports = function(RED) {
+    function AntennaGeniusBandLabels(config) {
+        RED.nodes.createNode(this,config);
+
+        var node = this;
+        this.bandNameA = "";
+        this.bandNameB = "";
+        this.server = RED.nodes.getNode(config.server);
+
+        this.server.client.on('connect', () => {
+            this.status({fill:"green",shape:"dot",text:"connected"});
+        });
+
+        this.server.client.on('error', () => {
+            this.status({fill:"red",shape:"ring",text:"disconnected"});        
+        });
+
+        this.server.updatesEventEmitter.on("status", () => {
+            let bandIndexA = this.server.status.portA_band;
+            let bandIndexB = this.server.status.portB_band;
+
+            let bandNameA = this.server.bands[bandIndexA].band_name;
+            let bandNameB = this.server.bands[bandIndexB].band_name;
+
+            let changed = false;
+            if(bandNameA !== this.bandNameA) {
+                changed = true;
+                this.bandNameA = bandNameA;
+            }
+            if(bandNameB !== this.bandNameB) {
+                changed = true;
+                this.bandNameB = bandNameB;
+            }
+            if(changed) {
+                node.send({payload: {bandLabelA: bandNameA, bandLabelB: bandNameB}});
+            }
+        });
+
+        this.server.updatesEventEmitter.on("antennas", () => {
+//            console.log('antennas');
+        });
+
+        this.server.updatesEventEmitter.on("bands", () => {
+//            console.log('bands');
+        });
+
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("antenna-genius-band-labels",AntennaGeniusBandLabels);
+}

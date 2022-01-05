81891d867b4d41184572a15c3d30e4fccac874cb
sm6srw <sm6srw@arrl.net>
2022-01-05 17:10:08 -0500

    Finalize status node

--- antenna-genius-antenna-status.js
+++ antenna-genius-antenna-status.js
@@ -4,6 +4,18 @@ module.exports = function(RED) {
 
         var node = this;
         this.antennaNumber = config.antennaNb;
+        this.radioA = {
+            selected: false,
+            enabled: false,
+            name: "",
+            background: ""
+        }
+        this.radioB = {
+            selected: false,
+            enabled: false,
+            name: "",
+            background: ""
+        }
         this.server = RED.nodes.getNode(config.server);
 
         this.server.client.on('connect', () => {
@@ -15,8 +27,70 @@ module.exports = function(RED) {
         });
 
         this.server.updatesEventEmitter.on("status", () => {
-            console.log('status');
-            let anb = this.antennaNumber;
+            let changed = false;
+            // Selected?
+            let radioAselected = this.antennaNumber == this.server.status.portA_antenna;
+            if(radioAselected != this.radioA.selected) {
+                changed = true;
+                this.radioA.selected = radioAselected;
+            }
+
+            let radioBselected = this.antennaNumber == this.server.status.portB_antenna;
+            if(radioBselected != this.radioB.selected) {
+                changed = true;
+                this.radioB.selected = radioBselected;
+            }
+
+            // Enabled?
+            let radioAenabled = this.server.antennas[this.antennaNumber - 1].antenna_bands[this.server.status.portA_band] == 1;
+            if(radioAenabled != this.radioA.enabled) {
+                changed = true;
+                this.radioA.enabled = radioAenabled;
+            }
+
+            let radioBenabled = this.server.antennas[this.antennaNumber - 1].antenna_bands[this.server.status.portB_band] == 1;
+            if(radioBenabled != this.radioB.enabled) {
+                changed = true;
+                this.radioB.enabled = radioBenabled;
+            }
+
+            // Get name
+            let name = this.server.antennas[this.antennaNumber - 1].antenna_name;
+            if(this.radioA.name !== name) {
+                changed = true;
+                this.radioA.name = this.radioB.name = name;
+            }
+
+            if(radioAenabled) {
+                if(radioAselected) {
+                    this.radioA.background = "blue";
+                }
+                else {
+                    this.radioA.background = "green";
+                }
+            } 
+            else {
+                this.radioA.background = "gray";
+            }
+
+            if(radioBenabled) {
+                if(radioBselected) {
+                    this.radioB.background = "blue";
+                }
+                else {
+                    this.radioB.background = "green";
+                }
+            } 
+            else {
+                this.radioB.background = "gray";
+            }
+
+            let radioATopic = "1;" + this.antennaNumber;
+            let radioBTopic = "2;" + this.antennaNumber;
+
+            if(changed) {
+                node.send([{payload: this.radioA, enabled: this.radioA.enabled, topic: radioATopic}, {payload: this.radioB, enabled: this.radioB.enabled, topic: radioBTopic}]);
+            }
         });
 
         this.server.updatesEventEmitter.on("antennas", () => {
